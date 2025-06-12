da91e3972a878a9d79b8c51d61ccff383bba3f54
Rohan25096 <rohansahoo2509@gmail.com>
2025-06-12 17:25:53 +0530

    small_update.

--- OHLCHistorical/main.py
+++ OHLCHistorical/main.py
@@ -1,26 +1,33 @@
 from OHLC_fetcher import get_historical_ohlc
 import pandas as pd
+def get_hist_ohlc(exchange,token_no,interval,from_date,to_date):
+    
+    data = get_historical_ohlc(
+        exchange=exchange,
+        symboltoken=token_no,  # replace with real token
+        interval=interval,
+        from_date=from_date,
+        to_date=to_date
+    )
 
-data = get_historical_ohlc(
-    exchange="NSE",
-    symboltoken="4503",  # replace with real token
-    interval="ONE_HOUR",
-    from_date="2025-01-06 09:15",
-    to_date="2025-06-12 15:30"
-)
+    df = pd.DataFrame(data)
 
-df = pd.DataFrame(data)
+    # Rename columns directly
+    df.columns = ['DateTime', 'Open', 'High', 'Low', 'Close', 'Volume']
 
-# Rename columns directly
-df.columns = ['DateTime', 'Open', 'High', 'Low', 'Close', 'Volume']
+    # Convert DateTime column to datetime dtype
+    df['DateTime'] = pd.to_datetime(df['DateTime'])
 
-# Convert DateTime column to datetime dtype
-df['DateTime'] = pd.to_datetime(df['DateTime'])
+    print(df)
 
-print(df)
+    # Save DataFrame to CSV file
+    csv_file_path = f'Data/historical_data_{token_no}.csv'  # update path as needed
+    df.to_csv(csv_file_path, index=False)
 
-# Save DataFrame to CSV file
-csv_file_path = 'Data/historical_data_MPHASIS.csv'  # update path as needed
-df.to_csv(csv_file_path, index=False)
+    print(f"Data saved to {csv_file_path}")
 
-print(f"Data saved to {csv_file_path}")
+get_hist_ohlc(exchange="NSE",
+        token_no="2263",  # replace with real token
+        interval="FIFTEEN_MINUTE",
+        from_date="2025-06-01 09:15",
+        to_date="2025-06-12 15:30")
