a1f8ac61b9c5dd11b9f6036df894691a0f0d1048
Rohan25096 <rohansahoo2509@gmail.com>
2025-06-14 16:35:57 +0530

    Integrating Supabase for logging live market data.

---
+++ OHLCHistorical/database_connection.py
@@ -0,0 +1,17 @@
+from supabase import create_client, Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv(dotenv_path="Your path to the .env file")
+
+project_url = os.getenv("supabaseUrl")
+project_key = os.getenv("supabase_key")
+
+#Url and API parsing.
+url: str = project_url
+key: str = project_key
+
+#Creating client session.
+supabase : Client = create_client(url,key)
+
+print(supabase.table("tick_data").select("*").limit(1).execute().data)
